6e3f76aace0c0d34fcec48cd19c58b6e20f38b80
RomanParaskiva <parasta2018@gmail.com>
2022-07-29 10:26:24 +0300

    fix discover comp on mobile

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -9,13 +9,9 @@ const Discover = () => {
   
   const { topic } = router.query
   
-  const activeTopicStyle = `xl:border-2 hover:bg-primary
-  xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full
-   flex items-center gap-2 justify-center cursor-pointer text-[#F51997]`
+  const activeTopicStyle = `xl:border-[#F51997] text-[#F51997]`
 
-  const topicStyle =`xl:border-2 hover:bg-primary
-   xl:border-gray-300 px-3 py-2 rounded xl:rounded-full
-    flex items-center gap-2 justify-center cursor-pointer text-black`
+  const topicStyle =`xl:border-gray-300 text-black`
 
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
@@ -25,11 +21,13 @@ const Discover = () => {
       <div className='flex gap-3 flex-wrap'>
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
-            <div className={topic == item.name ? activeTopicStyle : topicStyle}>
+            <div className={`xl:border-2 hover:bg-primary 
+            px-3 py-2 rounded xl:rounded-full flex flex-col md:flex-row items-center 
+            gap-2 justify-center cursor-pointer` + topic == item.name ? activeTopicStyle : topicStyle}>
               <span className='font-bold text-2xl xl:text-md'>
                 {item.icon}
               </span>
-              <span className='font-medium text-md hidden xl:block capitalize'>
+              <span className='font-medium text-md capitalize'>
                 {item.name}
               </span>
             </div>
